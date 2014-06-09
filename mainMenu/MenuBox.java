cf021ea20a2f6085796e2be435d477cf340b67aa
whitefang57 <lemon_57@yahoo.com>
2014-06-09 11:51:15 -0700

    made menu neater

--- mainMenu/MenuBox.java
+++ mainMenu/MenuBox.java
@@ -6,7 +6,7 @@ import java.awt.*;
 public class MenuBox extends JFrame {
 	public MenuBox() {
 		setTitle("Main Menu");
-		setSize(250, 350);
+		setSize(250, 225);
 		setLocationRelativeTo(null);
 		JPanel panel = new JPanel();
 		panel.setLayout(new BorderLayout());
@@ -20,32 +20,42 @@ public class MenuBox extends JFrame {
 		JButton configButton = new JButton("Options");
 		JButton quitButton = new JButton("Quit");
 
+
 		logoLabel.setAlignmentX(JLabel.CENTER_ALIGNMENT);
-		duelButton.setAlignmentX(JButton.CENTER_ALIGNMENT);
-		deckEditButton.setAlignmentX(JButton.CENTER_ALIGNMENT);
-		shopPacksButton.setAlignmentX(JButton.CENTER_ALIGNMENT);
-		shopDecksButton.setAlignmentX(JButton.CENTER_ALIGNMENT);
-		configButton.setAlignmentX(JButton.CENTER_ALIGNMENT);
-		quitButton.setAlignmentX(JButton.CENTER_ALIGNMENT);
 
-		JPanel buttonPane = new JPanel();
-		buttonPane.setLayout(new BoxLayout(buttonPane, BoxLayout.Y_AXIS));
+		duelButton.setAlignmentX(JButton.RIGHT_ALIGNMENT);
+		shopPacksButton.setAlignmentX(JButton.RIGHT_ALIGNMENT);
+		configButton.setAlignmentX(JButton.RIGHT_ALIGNMENT);
 
-		buttonPane.add(logoLabel);
-		buttonPane.add(Box.createVerticalStrut(10));
-		buttonPane.add(duelButton);
-		buttonPane.add(Box.createVerticalStrut(10));
-		buttonPane.add(deckEditButton);
-		buttonPane.add(Box.createVerticalStrut(10));
-		buttonPane.add(shopPacksButton);
-		buttonPane.add(Box.createVerticalStrut(10));
-		buttonPane.add(shopDecksButton);
-		buttonPane.add(Box.createVerticalStrut(10));
-		buttonPane.add(configButton);
-		buttonPane.add(Box.createVerticalStrut(10));
-		buttonPane.add(quitButton);
+		deckEditButton.setAlignmentX(JButton.LEFT_ALIGNMENT);
+		shopDecksButton.setAlignmentX(JButton.LEFT_ALIGNMENT);
+		quitButton.setAlignmentX(JButton.LEFT_ALIGNMENT);
 
-		buttonPane.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
-		panel.add(buttonPane, BorderLayout.CENTER);
+
+		JPanel buttonPaneTop = new JPanel();
+		buttonPaneTop.setLayout(new BoxLayout(buttonPaneTop, BoxLayout.Y_AXIS));
+		buttonPaneTop.add(logoLabel);
+		buttonPaneTop.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+		panel.add(buttonPaneTop,BorderLayout.PAGE_START);
+
+		JPanel buttonPaneLeft = new JPanel();
+		buttonPaneLeft.setLayout(new BoxLayout(buttonPaneLeft, BoxLayout.Y_AXIS));
+		buttonPaneLeft.add(duelButton);
+		buttonPaneLeft.add(Box.createVerticalStrut(5));
+		buttonPaneLeft.add(shopPacksButton);
+		buttonPaneLeft.add(Box.createVerticalStrut(5));
+		buttonPaneLeft.add(configButton);
+		buttonPaneLeft.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+		panel.add(buttonPaneLeft, BorderLayout.LINE_START);
+
+		JPanel buttonPaneRight = new JPanel();
+		buttonPaneRight.setLayout(new BoxLayout(buttonPaneRight, BoxLayout.Y_AXIS));
+		buttonPaneRight.add(deckEditButton);
+		buttonPaneRight.add(Box.createVerticalStrut(5));
+		buttonPaneRight.add(shopDecksButton);
+		buttonPaneRight.add(Box.createVerticalStrut(5));
+		buttonPaneRight.add(quitButton);
+		buttonPaneRight.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+		panel.add(buttonPaneRight, BorderLayout.LINE_END);
 	}
 }
